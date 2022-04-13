2b10a69bddb98feb2360e6e8ad0eecde8904776f
Matthew Baugh <mb4617@ic.ac.uk>
2022-04-13 17:36:41 +0100

    Add setup.py for package installation

---
+++ setup.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+from setuptools import setup
+
+with open(Path(__file__).parent / 'README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='pie-torch',
+    version='0.0',
+    packages=['pietorch'],
+    url='https://github.com/matt-baugh/pytorch-poisson-image-editing',
+    license='',
+    author='Matthew Baugh',
+    author_email='matthew.baugh17@imperial.ac.uk',
+    description='N-dimensional Poisson image editing implemented with Pytorch',
+    long_description=long_description,
+    install_requirements=[
+        'torch>=1.7',  # torch.fft.fftn was added in 1.7, haven't actually tested lol.
+        'numpy'
+    ]
+)
